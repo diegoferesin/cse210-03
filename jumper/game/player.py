b5fc7986e76fd34d897d822526e809bb779c9689
Emma Lund <emma.lund22@gmail.com>
2022-05-20 00:02:48 -0600

    player class

---
+++ jumper/game/player.py
@@ -0,0 +1,18 @@
+from game.word import Word
+
+class Player:
+    def __init__(self):
+        self._lives = 8
+        self._letters_used = []
+
+    def get_lives(self):
+        return self._lives
+
+    def set_lives(self):
+        self._lives -= 1
+
+    def get_letters_used(self):
+        return self._letters_used
+
+    def add_letters_used(self,letter):
+        self._letters_used.append(letter)
